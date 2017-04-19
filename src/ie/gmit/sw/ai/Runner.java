52114a2583ae8f71ec6291650c5fec560b56f1a6
paulyd123 <g00297086@gmit.ie>
2017-04-19 21:34:07 +0100

    Added Runner class from previous lab and empty Fuzzy logig file

---
+++ src/ie/gmit/sw/ai/Runner.java
@@ -0,0 +1,28 @@
+package ie.gmit.sw.ai;
+
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.FunctionBlock;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Variable;
+
+public class Runner {
+	
+	public static void main(String[] args) {
+		String fileName = "fcl/funding.fcl";
+		FIS fis = FIS.load(fileName, true);
+		
+		FunctionBlock fb  = fis.getFunctionBlock("Project");
+		JFuzzyChart.get().chart(fb);
+		
+		fis.setVariable("funding", 38);
+		fis.setVariable("staffing", 9);
+		fis.evaluate();
+		
+        Variable risk = fb.getVariable("risk");
+        JFuzzyChart.get().chart(risk, risk.getDefuzzifier(), true);
+
+        System.out.println("Risk: " + fis.getVariable("risk").getValue() + "%"); //Output end result
+        JFuzzyChart.get().chart(risk, true);
+	}
+
+}
