3cdb733f99278de2b1e2769daa16bdd6561a1918
paulyd123 <g00297086@gmit.ie>
2017-04-19 15:00:31 +0100

    Node incorporated into Maze

--- src/ie/gmit/sw/ai/Maze.java
+++ src/ie/gmit/sw/ai/Maze.java
@@ -10,21 +10,36 @@ public class Maze {
 		init();
 		buildMaze();
 		
-		int featureNumber = (int)((dimension * dimension) * 0.01);
-		addFeature('\u0031', '0', featureNumber); //1 is a sword, 0 is a hedge
-		addFeature('\u0032', '0', featureNumber); //2 is help, 0 is a hedge
-		addFeature('\u0033', '0', featureNumber); //3 is a bomb, 0 is a hedge
-		addFeature('\u0034', '0', featureNumber); //4 is a hydrogen bomb, 0 is a hedge
+		//int featureNumber = (int)((dimension * dimension) * 0.01);
+		//addFeature('\u0031', '0', featureNumber); //1 is a sword, 0 is a hedge
+		//addFeature('\u0032', '0', featureNumber); //2 is help, 0 is a hedge
+		//addFeature('\u0033', '0', featureNumber); //3 is a bomb, 0 is a hedge
+		//addFeature('\u0034', '0', featureNumber); //4 is a hydrogen bomb, 0 is a hedge
+		int featureNumber = 5;
+		addFeature(1, 0, featureNumber); //1 is a sword, 0 is a hedge
+		addFeature(2, 0, featureNumber); //2 is help, 0 is a hedge
+		addFeature(3, 0, featureNumber); //3 is a bomb, 0 is a hedge
+		addFeature(4, 0, featureNumber); //4 is a hydrogen bomb, 0 is a hedge
 		
-		featureNumber = (int)((dimension * dimension) * 0.01);
-		addFeature('\u0036', '0', featureNumber); //6 is a Black Spider, 0 is a hedge
-		addFeature('\u0037', '0', featureNumber); //7 is a Blue Spider, 0 is a hedge
-		addFeature('\u0038', '0', featureNumber); //8 is a Brown Spider, 0 is a hedge
-		addFeature('\u0039', '0', featureNumber); //9 is a Green Spider, 0 is a hedge
-		addFeature('\u003A', '0', featureNumber); //: is a Grey Spider, 0 is a hedge
-		addFeature('\u003B', '0', featureNumber); //; is a Orange Spider, 0 is a hedge
-		addFeature('\u003C', '0', featureNumber); //< is a Red Spider, 0 is a hedge
-		addFeature('\u003D', '0', featureNumber); //= is a Yellow Spider, 0 is a hedge
+		//featureNumber = (int)((dimension * dimension) * 0.01);
+		//addFeature('\u0036', '0', featureNumber); //6 is a Black Spider, 0 is a hedge
+		//addFeature('\u0037', '0', featureNumber); //7 is a Blue Spider, 0 is a hedge
+		//addFeature('\u0038', '0', featureNumber); //8 is a Brown Spider, 0 is a hedge
+		//addFeature('\u0039', '0', featureNumber); //9 is a Green Spider, 0 is a hedge
+		//addFeature('\u003A', '0', featureNumber); //: is a Grey Spider, 0 is a hedge
+		//addFeature('\u003B', '0', featureNumber); //; is a Orange Spider, 0 is a hedge
+		//addFeature('\u003C', '0', featureNumber); //< is a Red Spider, 0 is a hedge
+		//addFeature('\u003D', '0', featureNumber); //= is a Yellow Spider, 0 is a hedge
+		
+		featureNumber = 20;
+		addFeature(6, 1, featureNumber); //6 is a Black Spider, 0 is a hedge
+		addFeature(7, 1, featureNumber); //7 is a Blue Spider, 0 is a hedge
+		addFeature(8, 1, featureNumber); //8 is a Brown Spider, 0 is a hedge
+		addFeature(9, 1, featureNumber); //9 is a Green Spider, 0 is a hedge
+		addFeature(10, 1, featureNumber); //: is a Grey Spider, 0 is a hedge
+		addFeature(11, 1, featureNumber); //; is a Orange Spider, 0 is a hedge
+		addFeature(12, 1, featureNumber); //< is a Red Spider, 0 is a hedge
+		addFeature(13, 1, featureNumber); //= is a Yellow Spider, 0 is a hedge
 	}
 	
 	private void init(){
@@ -59,23 +74,28 @@ public class Maze {
 			for (int col = 1; col < maze[row].length - 1; col++){
 				int num = (int) (Math.random() * 10);
 				if (num > 5 && col + 1 < maze[row].length - 1){
-					maze[row][col + 1] = '\u0020'; //\u0020 = 0x20 = 32 (base 10) = SPACE
+					//maze[row][col + 1] = '\u0020'; //\u0020 = 0x20 = 32 (base 10) = SPACE
+					maze[row][col + 1].setId(1);
 				}else{
-					if (row + 1 < maze.length - 1)maze[row + 1][col] = '\u0020';
+					//if (row + 1 < maze.length - 1)maze[row + 1][col] = '\u0020';
+					if (row + 1 < maze.length - 1)maze[row + 1][col].setId(1);
 				}
 			}
 		}		
 	}
 	
-	public char[][] getMaze(){
+	//public char[][] getMaze(){
+	public Node[][] getMaze(){
 		return this.maze;
 	}
 	
-	public char get(int row, int col){
+	//public char get(int row, int col){
+	public Node get(int row, int col){
 		return this.maze[row][col];
 	}
 	
-	public void set(int row, int col, char c){
+	//public void set(int row, int col, char c){
+	public void set(int row, int col, Node c){
 		this.maze[row][col] = c;
 	}
 	
