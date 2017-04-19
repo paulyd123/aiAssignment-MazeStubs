b47166cd2626d4889b0e3e90a2e537e0fc634273
paulyd123 <g00297086@gmit.ie>
2017-04-19 13:36:19 +0100

    Added class to move spider

---
+++ src/ie/gmit/sw/ai/MoveSpider.java
@@ -0,0 +1,10 @@
+package ie.gmit.sw.ai;
+
+public class MoveSpider extends Node{
+
+	public MoveSpider(int row, int col, int id) {
+		super(row, col, id);
+		// TODO Auto-generated constructor stub
+	}
+
+}
