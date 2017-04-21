f0d3b85f379e59340a922cc881e3aa1eff16ee39
paulyd123 <g00297086@gmit.ie>
2017-04-21 15:07:00 +0100

    Populating Player

--- src/ie/gmit/sw/ai/Player.java
+++ src/ie/gmit/sw/ai/Player.java
@@ -1,5 +1,30 @@
 package ie.gmit.sw.ai;
 
 public class Player {
+	
+	private int row;
+	private int col;
+	
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+	
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public Player(int row, int col){
+		this.row = row;
+		this.col = col;
+	}
+
 
 }
